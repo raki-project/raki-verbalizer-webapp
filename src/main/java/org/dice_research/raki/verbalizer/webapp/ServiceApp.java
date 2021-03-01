9daa08934968e7681bc27b7c894d0b3e6d4e8a44
Rene Speck <speckrene@gmail.com>
2021-03-01 13:52:37 +0100

    Adds shutdown file writer.

--- src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
+++ src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
@@ -1,11 +1,26 @@
 package org.dice_research.raki.verbalizer.webapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
+/**
+ *
+ * @author rspeck
+ *
+ */
 @SpringBootApplication
 public class ServiceApp {
   public static void main(final String[] args) {
+
+    final SpringApplicationBuilder app =
+        new SpringApplicationBuilder(ServiceApp.class).web(WebApplicationType.NONE);
+    app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+    app.run(args);
+
+    // app.application().run(args);
     SpringApplication.run(ServiceApp.class, args);
   }
 }
