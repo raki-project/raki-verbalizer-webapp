43fe3968b5de47bc1e3f97f58cb9eaf73fd2347d
Rene Speck <speckrene@gmail.com>
2022-01-14 17:28:29 +0100

    Adds Cors mappings.

--- src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
+++ src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
@@ -7,18 +7,20 @@ import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SolrAutoConfiguration.class)
 public class ServiceApp extends SpringBootServletInitializer {
 
   public static void main(final String[] args) {
 
-    final SpringApplication springApplication = new SpringApplicationBuilder(ServiceApp.class)//
+    new SpringApplicationBuilder(ServiceApp.class)//
+        .listeners(new ApplicationPidFileWriter(".shutdown.pid"))//
         .web(WebApplicationType.NONE)//
-        .build();
-
-    springApplication.addListeners(new ApplicationPidFileWriter(".shutdown.pid"));
-    springApplication.run(args);
+        .build()//
+        .run(args);
 
     SpringApplication.run(ServiceApp.class, args);
   }
@@ -26,5 +28,20 @@ public class ServiceApp extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
     return builder.sources(ServiceApp.class);
+  }
+
+  /**
+   * Makes the app public to each origin request.
+   *
+   * @return
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("*");
+      }
+    };
   }
 }
