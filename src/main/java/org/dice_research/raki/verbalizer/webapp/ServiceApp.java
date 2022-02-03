ed6447c9adae94dd668d779d8cce7017d69a08ab
Rene Speck <speckrene@gmail.com>
2022-02-03 06:42:11 +0100

    Removes Const class.

--- src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
+++ src/main/java/org/dice_research/raki/verbalizer/webapp/ServiceApp.java
@@ -1,5 +1,9 @@
 package org.dice_research.raki.verbalizer.webapp;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +15,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ *
+ * @author rspeck
+ *
+ */
 @SpringBootApplication(exclude = SolrAutoConfiguration.class)
 public class ServiceApp extends SpringBootServletInitializer {
 
+  // folder to store files
+  public static final Path tmp;
+  static {
+    tmp = Paths.get(System.getProperty("java.io.tmpdir")//
+        .concat(File.separator)//
+        .concat("raki"));
+
+    if (!tmp.toFile().exists()) {
+      tmp.toFile().mkdirs();
+      tmp.toFile().deleteOnExit();
+    }
+  }
+
+  /**
+   * ServiceApp entry point.
+   *
+   * @param args
+   */
   public static void main(final String[] args) {
 
     new SpringApplicationBuilder(ServiceApp.class)//
