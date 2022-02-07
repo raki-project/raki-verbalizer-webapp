202daabed58dfa8bdd28410e26e80a65eb8e1d19
Rene Speck <speckrene@gmail.com>
2022-02-07 18:14:10 +0100

    Updates test.

--- src/test/java/org/dice_research/raki/verbalizer/webapp/ApplicationTests.java
+++ src/test/java/org/dice_research/raki/verbalizer/webapp/ApplicationTests.java
@@ -3,12 +3,12 @@ package org.dice_research.raki.verbalizer.webapp;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.dice_research.raki.verbalizer.webapp.controller.VerbalizerController;
 import org.dice_research.raki.verbalizer.webapp.handler.VerbalizerResults;
 import org.junit.jupiter.api.Test;
@@ -16,28 +16,29 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ApplicationTests {
-  private static final Logger LOG = LogManager.getLogger(ApplicationTests.class);
 
+  // private static final Logger LOG = LogManager.getLogger(ApplicationTests.class);
+
+  final VerbalizerController ctlr = new VerbalizerController();
+
+  String koala = "src/main/resources/static/ontology/koala.owl";
+
+  /**
+   * Rules test with 1 file for axioms and ontology parameter.
+   *
+   * @throws IOException
+   * @throws FileNotFoundException
+   */
   @Test
-  public void koalaTest() {
-    try {
-      final VerbalizerController ctlr = new VerbalizerController();
+  public void rulesTest() throws FileNotFoundException, IOException {
 
-      final Path axiomsPath = Paths.get("koala.owl");
-      final Path ontologyPath = Paths.get("koala.owl");
+    final Path axiomsPath = Paths.get(koala);
+    final MultipartFile axioms = new MockMultipartFile(axiomsPath.toFile().getName(),
+        new FileInputStream(axiomsPath.toFile()));
 
-      final MultipartFile axioms = new MockMultipartFile(axiomsPath.toFile().getPath(),
-          new FileInputStream(axiomsPath.toFile()));
+    final VerbalizerResults results = ctlr.rules(axioms, Optional.of(axioms), Optional.of(""));
 
-      final MultipartFile ontology = new MockMultipartFile(ontologyPath.toFile().getPath(),
-          new FileInputStream(ontologyPath.toFile()));
-
-      final VerbalizerResults results = ctlr.rules(axioms, ontology);
-
-      assertTrue(results.response.size() > 0);
-      assertTrue(results.response.size() == 36);
-    } catch (final IOException e) {
-      LOG.error(e.getLocalizedMessage(), e);
-    }
+    assertTrue(results.response.size() > 0);
+    assertTrue(results.response.size() == 42);
   }
 }
