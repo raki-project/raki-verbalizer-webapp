f7bb6a1c5a22ef163dfc64bb60eccd41cbc66183
Rene Speck <speckrene@gmail.com>
2022-11-01 15:46:59 +0100

    Updates controller.

--- src/main/java/org/dice_research/raki/verbalizer/webapp/controller/MultipartFileHelper.java
+++ src/main/java/org/dice_research/raki/verbalizer/webapp/controller/MultipartFileHelper.java
@@ -11,8 +11,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dice_research.raki.verbalizer.webapp.ServiceApp;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ */
 public class MultipartFileHelper {
 
   protected static final Logger LOG = LogManager.getLogger(MultipartFileHelper.class);
@@ -22,38 +25,46 @@ public class MultipartFileHelper {
    *
    * @param file
    * @return
+   * @throws IOException
+   * @throws IllegalStateException
+   * @throws FileNotFoundException
    */
-  public MultipartFile removeImports(final MultipartFile file) {
+  public MultipartFile removeImports(final MultipartFile file)
+      throws FileNotFoundException, IllegalStateException, IOException {
     String content = "";
-    try {
-      content = new PrePro().removeImports(fileUpload(file, ServiceApp.tmp));
-    } catch (final FileNotFoundException e) {
-      LOG.error(e.getLocalizedMessage(), e);
-    }
+    content = new PrePro().removeImports(fileUpload(file, ServiceApp.tmp));
     return new MockMultipartFile(//
         file.getName(), file.getName(), file.getContentType(), content.getBytes());
   }
 
   /**
-   * Uploads the given file to the given folder.
+   * Uploads the given file to the given folder and renames the file with a time stamp prefix.
+   * Deletes all uploaded files on exit.
    *
    * @param file
    * @param folder
    * @return Path of the stored file
+   * @throws IOException
+   * @throws IllegalStateException
    */
-  public Path fileUpload(final MultipartFile file, final Path folder) {
+  public Path fileUpload(final MultipartFile file, final Path folder)
+      throws IllegalStateException, IOException {
     final Path path = Paths.get(folder.toFile()//
         .getAbsolutePath()//
         .concat(File.separator)//
         .concat(String.valueOf(new Timestamp(System.currentTimeMillis()).getTime()))//
         .concat("_")//
         .concat(file.getOriginalFilename()));
-    try {
-      file.transferTo(path.toFile());
-      path.toFile().deleteOnExit();
-    } catch (final IOException e) {
-      LOG.error(e.getLocalizedMessage(), e);
-    }
+    file.transferTo(path.toFile());
+    path.toFile().deleteOnExit();
     return path;
+  }
+
+  public MultipartFile getXMLMultipartFile(final byte[] b) {
+    return new MockMultipartFile(//
+        String.valueOf(new Timestamp(System.currentTimeMillis()).getTime()), //
+        null, //
+        MimeTypeUtils.APPLICATION_XML.getType(), //
+        b);
   }
 }
